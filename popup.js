ea788d9aa3102d56d58e4893f3202af988300828
NeONRAcE <rayan-cooper@mail.ru>
2017-05-22 10:37:44 +0300

    Show Result func fix

--- popup.js
+++ popup.js
@@ -11,6 +11,9 @@ function ShowProgressMessage(msg) {
     function showResults() {
         var duration = (endTime - startTime) / 1000;
         var bitsLoaded = downloadSize * 8;
+        var speedBps = (bitsLoaded / duration).toFixed(2);
+        var speedKbps = (speedBps / 1024).toFixed(2);
+        var speedMbps = (speedKbps / 1024).toFixed(2);
         ShowProgressMessage([
             "Speed"
         ]);
