880a11b73360035ae1f7e5c1c26351eef535a63f
NeONRAcE <rayan-cooper@mail.ru>
2017-05-26 00:55:08 +0300

    Comments (lab5)

--- popup.js
+++ popup.js
@@ -1,6 +1,26 @@
+/**
+* Адрес загружаемого изображения
+*
+* @var string $imageAddr
+*/
 var imageAddr = "http://www.kenrockwell.com/contax/images/g2/examples/31120037-5mb.jpg"; 
+
+/**
+* Точный размер загружаемого изображения
+*
+* @var int $downloadSize
+*/
 var downloadSize = 4995374; //bytes
 
+/**
+* Работа со свойством {@link $msg}
+*
+* Если аргумент определен, то устанавливается новое
+* значение свойства msg
+* (отслеживание пргресса проверки скорости)
+*
+* @param string $msg сообщение
+*/
 function ShowProgressMessage(msg) {
     var oProgress = document.getElementById("progress");
     if (oProgress) {
@@ -8,6 +28,9 @@ function ShowProgressMessage(msg) {
     }
 }
 
+/**
+* Функция отображения статуса проверки скорости
+*/
 function InitiateSpeedDetection() {
     ShowProgressMessage("Loading the image, please wait...");
     window.setTimeout(MeasureConnectionSpeed, 1);
@@ -27,10 +50,16 @@ function MeasureConnectionSpeed() {
         showResults();
     }
 
+/**
+* Обработчик ошибки загрузки изображения
+*/
     download.onerror = function (err, msg) {
         ShowProgressMessage("Invalid image, or error downloading");
     }
 
+/**
+* Алгоритм проверки скорости
+*/
     function showResults() {
         var duration = (endTime - startTime) / 1000;
         var bitsLoaded = downloadSize * 8;
