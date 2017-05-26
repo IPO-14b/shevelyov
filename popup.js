48908dcee5e59d5f5dad08bdc73cfd20bbad2fa5
NeONRAcE <rayan-cooper@mail.ru>
2017-05-27 00:37:38 +0300

    Lab 5 fixed

--- popup.js
+++ popup.js
@@ -22,6 +22,12 @@ var downloadSize = 4995374; //bytes
 * @param string $msg сообщение
 */
 function ShowProgressMessage(msg) {
+    
+    /**
+    *Обработчик, позволяющий взаимодействовать с элементом HTML страницы с ID=progress
+    *
+    * @var obj $oProgress
+    */
     var oProgress = document.getElementById("progress");
     if (oProgress) {
         oProgress.innerHTML = msg;
@@ -42,8 +48,30 @@ if (window.addEventListener) {
     window.attachEvent('onload', InitiateSpeedDetection);
 }
 
+/**
+* Функция, которая фиксирует время завершения загрузки изображения
+*/
 function MeasureConnectionSpeed() {
-    var startTime, endTime;
+    
+    /**
+    * Время начала загрузки
+    *
+    * @var int $startTime
+    */
+    var startTime;
+    
+    /**
+    * Время конца загрузки
+    *
+    * @var int $endTime
+    */
+    var endTime;
+    
+    /**
+    * Объект класса Image
+    *
+    * @var obj $download
+    */
     var download = new Image();
     download.onload = function () {
         endTime = (new Date()).getTime();
@@ -56,21 +84,51 @@ function MeasureConnectionSpeed() {
     download.onerror = function (err, msg) {
         ShowProgressMessage("Invalid image, or error downloading");
     }
+}
 
 /**
 * Алгоритм проверки скорости
 */
-    function showResults() {
-        var duration = (endTime - startTime) / 1000;
-        var bitsLoaded = downloadSize * 8;
-        var speedBps = (bitsLoaded / duration).toFixed(2);
-        var speedKbps = (speedBps / 1024).toFixed(2);
-        var speedMbps = (speedKbps / 1024).toFixed(2);
-        ShowProgressMessage([
-            "Your connection speed is:", 
-            speedBps + " bps", 
-            speedKbps + " kbps", 
-            speedMbps + " Mbps"
-        ]);
-    }
+function showResults() {
+    
+    /**
+    * Время загрузки изображения (в мс)
+    *
+    * @var float $duration
+    */
+    var duration = (endTime - startTime) / 1000;
+    
+    /**
+    * Количество загруженных байт
+    *
+    * @var int $bitesLoaded
+    */
+    var bitesLoaded = downloadSize * 8;
+    
+    /**
+    * Скорость в Байт/с
+    *
+    * @var float $speedBps
+    */
+    var speedBps = (bitesLoaded / duration).toFixed(2);
+    
+    /**
+    * Скорость в КБайт/с
+    *
+    * @var float $speedKbps
+    */
+    var speedKbps = (speedBps / 1024).toFixed(2);
+    
+    /**
+    * Скорость в МБайт/с
+    *
+    * @var float $speedMbps
+    */
+    var speedMbps = (speedKbps / 1024).toFixed(2);
+    ShowProgressMessage([
+        "Your connection speed is:", 
+        speedBps + " bps", 
+        speedKbps + " kbps", 
+        speedMbps + " Mbps"
+    ]);
 }
